664225a20e7cd98b5208857dce43037e1fe13b4a
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-07-09 22:05:18 +0300

    выполнили задачу 53

--- Задача 53/Program.cs
+++ Задача 53/Program.cs
@@ -1,3 +1,45 @@
 ﻿/*
     Задача 53. Задайте двумерный массив. Напишите программу, которая меняет первую и последнюю строку менстами
 */
+
+int[,] GetArray() {
+    int[,] Array = new int[5, 5];
+    for (int j = 0; j < 5; j++)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Array[j, i] = new Random().Next(1, 11);
+            System.Console.Write(Array[j, i] + " ");
+        }
+        System.Console.WriteLine();
+    } 
+    return Array;
+}
+
+int[,] SwapArray(int[,] Array) {
+    int First = Array[0, 0];
+    int Last = Array[4, 4];
+    Array[0, 0] = Last;
+    Array[4, 4] = First;
+    return Array;
+}
+
+void PrintArray(int[,] Array) {
+    for (int j = 0; j < 5; j++)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            System.Console.Write(Array[j, i] + " ");
+        }
+        System.Console.WriteLine();
+    } 
+}
+
+void Main() {
+    int[,] Array = GetArray();
+    int[,] Swap = SwapArray(Array);
+    System.Console.WriteLine();
+    PrintArray(Swap);
+}
+
+Main();
